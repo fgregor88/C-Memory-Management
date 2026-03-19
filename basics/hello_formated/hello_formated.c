e317e9f9df0b13c7bbc897bc848b295bed68b4f6
Filip Gregor <39672950+fgregor88@users.noreply.github.com>
2026-03-19 14:13:24 +0100

    fixed file structure

---
+++ basics/hello_formated/hello_formated.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    //comment
+    int a = 4;
+    float x = 0.5;
+    char c = 'a';
+    char *str = "Hello, this is a string";
+
+    printf("hello\n");
+    printf("Variables:\n a = %d,\n x = %f,\n c = %c,\n str = %s\n", a, x, c, str);
+
+    return 0;
+}
